70a1d20c4ca0f9084dfe8a874a8b749ba7ad94fd
BonganiChauke <bonganichauke11@outlook.com>
2024-02-25 18:08:25 +0200

    Open Account Method Update
    Added an increment method by creating an instance of AtomicInteger class to increment the account number every time a user creates an account and prints the account details .

--- src/mybank/Account.java
+++ src/mybank/Account.java
@@ -3,29 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mybank;
+
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author Romeo
  */
 public class Account {
-    
+
+    private static final AtomicInteger count = new AtomicInteger(145396); // to increament the value 
+    private int accountNumber; // accountNumber to auto generated to the user
+
     //method to open an account
-    public void openAccount(Scanner input, String firstName, String lastName, String typeAccount){
-        
+    public void openAccount(Scanner input, String firstName, String lastName, String typeAccount) {
+
         //prompts user for first name
-        System.out.println("Enter first name: " + firstName);
-        input.next(firstName);
-        
+        System.out.println("Enter first name: ");
+        firstName = input.next();
+
         //prompts user for last name
-        System.out.println("Enter last name: " + lastName);
-        input.next(lastName);
+        System.out.println("Enter last name: ");
+        lastName = input.next();
         
-        //prompts user for type of account
-        System.out.println("Savigs or Cheque: " + typeAccount);
-        input.next(typeAccount);
+        int option = 0;
         
+        //do while to keep looping the prompt if selects an invalid input
+        do {
+            
+            //prompts user for type of account
+            System.out.println("Select an option :\n1. Savigs\n2. Cheque");
+            option = input.nextInt();
+
+            //if statement to assign a value to variable typeAccount
+            if (option == 1) {
+
+                typeAccount = "Savings";
+                
+            } else if (option == 2) {
+
+                typeAccount = "Cheque";
+                
+            } else {
+              
+                System.out.println("Invalid Option");
+            }
+           
+        }
+        while(option != 1 && option != 2);
+
+        accountNumber = count.incrementAndGet();// to increment the variable accountNumber using count AtomicInteger
+        System.out.println("\n*************************************************\n"
+                + "Account Succesfully Created\n"
+                + "First Name : " + firstName + "\n"
+                + "Last Name  : " + lastName + "\n"
+                + "Type of Account : " + typeAccount + "\n"
+                + "Account Number  : " + accountNumber + "\n");
+
     }
-    
+
 }
